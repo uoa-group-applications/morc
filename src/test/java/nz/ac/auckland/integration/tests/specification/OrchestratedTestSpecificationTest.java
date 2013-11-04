9b9bd8ffbc8461ec22041253cabc7b7a3eed997e
dmacdonald2013 <davidm@cdonald.net.nz>
2013-11-04 17:57:47 +1300

    added more tests

--- src/test/java/nz/ac/auckland/integration/tests/specification/OrchestratedTestSpecificationTest.java
+++ src/test/java/nz/ac/auckland/integration/tests/specification/OrchestratedTestSpecificationTest.java
@@ -1,7 +1,10 @@
 package nz.ac.auckland.integration.tests.specification;
 
+import nz.ac.auckland.integration.testing.endpointoverride.CxfEndpointOverride;
+import nz.ac.auckland.integration.testing.endpointoverride.EndpointOverride;
 import nz.ac.auckland.integration.testing.expectation.ExceptionMockExpectation;
 import nz.ac.auckland.integration.testing.specification.OrchestratedTestSpecification;
+import org.apache.camel.Endpoint;
 import org.apache.camel.ProducerTemplate;
 import org.junit.Assert;
 import org.junit.Test;
@@ -50,12 +53,38 @@ public class OrchestratedTestSpecificationTest extends Assert {
 
     @Test
     public void testAddEndpointOverride() throws Exception {
+        EndpointOverride e1 = new EndpointOverride() {
+            @Override
+            public void overrideEndpoint(Endpoint endpoint) { }
+        };
+        EndpointOverride e2 = new EndpointOverride() {
+            @Override
+            public void overrideEndpoint(Endpoint endpoint) { }
+        };
 
+        TestOrchestratedTestSpecification spec = new TestOrchestratedTestSpecification
+                .Builder("targetUri", "description")
+                .addEndpointOverride(e1)
+                .addEndpointOverride(e2)
+                .build();
+
+        assertEquals(3,spec.getEndpointOverrides().size());
+
+        assertTrue(spec.getEndpointOverrides().contains(e1));
+        assertTrue(spec.getEndpointOverrides().contains(e2));
+        spec.getEndpointOverrides().remove(e1);
+        spec.getEndpointOverrides().remove(e2);
+        assertTrue(spec.getEndpointOverrides().iterator().next() instanceof CxfEndpointOverride);
     }
 
     @Test
     public void testSleepForTestCompletion() throws Exception {
+        TestOrchestratedTestSpecification spec = new TestOrchestratedTestSpecification
+                .Builder("targetUri", "description")
+                .sleepForTestCompletion(1234)
+                .build();
 
+        assertEquals(1234,spec.getSleepForTestCompletion());
     }
 }
 
