182fbbad86e5ee8948aa1d6cf6201dff1946b8ad
dmacdonald2013 <davidm@cdonald.net.nz>
2013-11-06 16:46:36 +1300

    added some javadoc

--- src/main/java/nz/ac/auckland/integration/testing/validator/HttpExceptionValidator.java
+++ src/main/java/nz/ac/auckland/integration/testing/validator/HttpExceptionValidator.java
@@ -80,36 +80,57 @@ public class HttpExceptionValidator implements Validator {
         private Validator responseHeadersValidator;
         private int statusCode;
 
+        /**
+         * @param responseBodyValidator A validator for the expected error response body
+         */
         public Builder responseBodyValidator(Validator responseBodyValidator) {
             this.responseBodyValidator = responseBodyValidator;
             return this;
         }
 
+        /**
+         * @param resource An XML resource that is expected to be received in the body of the HTTP response
+         */
         public Builder responseBodyValidator(XmlTestResource resource) {
             this.responseBodyValidator = new XmlValidator(resource);
             return this;
         }
 
+        /**
+         * @param resource A JSON resource that is expected to be received in the body of the HTTP response
+         */
         public Builder responseBodyValidator(JsonTestResource resource) {
             this.responseBodyValidator = new JsonValidator(resource);
             return this;
         }
 
+        /**
+         * @param resource A plain text resource that is expected to be received in the body of the HTTP response
+         */
         public Builder responseBodyValidator(PlainTextTestResource resource) {
             this.responseBodyValidator = new PlainTextValidator(resource);
             return this;
         }
 
+        /**
+         * @param responseHeadersValidator A validator for the HTTP response headers
+         */
         public Builder responseHeadersValidator(Validator responseHeadersValidator) {
             this.responseHeadersValidator = responseHeadersValidator;
             return this;
         }
 
+        /**
+         * @param resource A resource containing the expected response headers
+         */
         public Builder responseHeadersValidator(HeadersTestResource resource) {
             this.responseHeadersValidator = new HeadersValidator(resource);
             return this;
         }
 
+        /**
+         * @param statusCode The expected response status code
+         */
         public Builder statusCode(int statusCode) {
             this.statusCode = statusCode;
             return this;
