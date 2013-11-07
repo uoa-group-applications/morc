f87872224da27279f4a01d63b60abb1985a902a6
dmacdonald2013 <d.macdonald@auckland.ac.nz>
2013-11-07 18:26:12 +1300

    initial tests

---
+++ src/test/java/nz/ac/auckland/integration/tests/resource/SoapFaultTestResourceTest.java
@@ -0,0 +1,49 @@
+package nz.ac.auckland.integration.tests.resource;
+
+import nz.ac.auckland.integration.testing.resource.SoapFaultTestResource;
+import nz.ac.auckland.integration.testing.resource.XmlTestResource;
+import nz.ac.auckland.integration.testing.utility.XMLUtilities;
+import nz.ac.auckland.integration.testing.validator.XmlValidator;
+import org.apache.cxf.binding.soap.SoapFault;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.xml.namespace.QName;
+
+public class SoapFaultTestResourceTest extends Assert {
+
+    @Test
+    public void testFaultCodeMessageValid() throws Exception {
+        SoapFaultTestResource resource = new SoapFaultTestResource(new QName("foo","baz"),"foo");
+
+        SoapFault fault = resource.getValue();
+        assertEquals(fault.getMessage(),"foo");
+        assertEquals(fault.getFaultCode(),new QName("foo","baz"));
+    }
+
+    @Test
+    public void testDetailWrapped() throws Exception {
+        XMLUtilities xmlUtilities = new XMLUtilities();
+        XmlTestResource detail = new XmlTestResource(xmlUtilities.getXmlAsDocument("<detail><foo/></detail>"));
+
+        SoapFaultTestResource resource = new SoapFaultTestResource(new QName("foo","baz"),"foo",detail);
+        SoapFault fault = resource.getValue();
+        assertEquals(fault.getMessage(),"foo");
+        assertEquals(fault.getFaultCode(),new QName("foo","baz"));
+        XmlValidator validator = new XmlValidator(detail);
+        assertTrue(validator.validate(xmlUtilities.getDocumentAsString(fault.getDetail())));
+    }
+
+    @Test
+    public void testDetailUnwrapped() throws Exception {
+        XMLUtilities xmlUtilities = new XMLUtilities();
+        XmlTestResource detail = new XmlTestResource(xmlUtilities.getXmlAsDocument("<foo/>"));
+
+        SoapFaultTestResource resource = new SoapFaultTestResource(new QName("foo","baz"),"foo",detail);
+        SoapFault fault = resource.getValue();
+        assertEquals(fault.getMessage(),"foo");
+        assertEquals(fault.getFaultCode(),new QName("foo","baz"));
+        XmlValidator validator = new XmlValidator(new XmlTestResource(xmlUtilities.getXmlAsDocument("<detail><foo/></detail>")));
+        assertTrue(validator.validate(xmlUtilities.getDocumentAsString(fault.getDetail())));
+    }
+}
