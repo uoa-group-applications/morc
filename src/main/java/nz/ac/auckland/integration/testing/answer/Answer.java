2c9b81eb94c15ffe62b1a97e7659c61ebd721f5c
dmacdonald2013 <d.macdonald@auckland.ac.nz>
2013-12-24 23:12:04 +1300

    added javadoc

--- src/main/java/nz/ac/auckland/integration/testing/answer/Answer.java
+++ src/main/java/nz/ac/auckland/integration/testing/answer/Answer.java
@@ -2,6 +2,15 @@ package nz.ac.auckland.integration.testing.answer;
 
 import org.apache.camel.Exchange;
 
+/**
+ * An interface to specify a particular response for an expectation
+ *
+ * @author David MacDonald <d.macdonald@auckland.ac.nz>
+ */
 public interface Answer<T> {
+    /**
+     * @param exchange  The exchange received by an expectation that will invoke a given response
+     * @return          A response that will be given back to the client
+     */
     public T response(Exchange exchange) throws Exception;
 }
