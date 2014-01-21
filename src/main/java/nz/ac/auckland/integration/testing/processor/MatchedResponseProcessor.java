3fbd6f98559e4da388c9a6d4826f47302460ab15
dmacdonald2013 <davidm@cdonald.net.nz>
2014-01-21 15:39:08 +1300

    clean up of code to ensure threadsafety

--- src/main/java/nz/ac/auckland/integration/testing/processor/MatchedResponseProcessor.java
+++ src/main/java/nz/ac/auckland/integration/testing/processor/MatchedResponseProcessor.java
@@ -1,61 +1,55 @@
 package nz.ac.auckland.integration.testing.processor;
 
-import nz.ac.auckland.integration.testing.resource.TestResource;
-import nz.ac.auckland.integration.testing.predicate.Validator;
 import org.apache.camel.Exchange;
 import org.apache.camel.Predicate;
 import org.apache.camel.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 /**
  * An answer that will return a response back to the client based on the incoming exchange message
  *
  * @author David MacDonald <d.macdonald@auckland.ac.nz>
  */
-public class MatchedResponseBodiesProcessor implements Processor {
+public class MatchedResponseProcessor implements Processor {
 
-    private static final Logger logger = LoggerFactory.getLogger(MatchedResponseBodiesProcessor.class);
-    private Collection<MatchedResponse> responses = new HashSet<>();
+    private static final Logger logger = LoggerFactory.getLogger(MatchedResponseProcessor.class);
+    private Collection<MatchedResponse> responses;
 
-    //todo add remove on match
-
-    public void addMatchedResponse(MatchedResponse matchedResponse) {
-        responses.add(matchedResponse);
+    public MatchedResponseProcessor(MatchedResponse... responses) {
+        this.responses = new HashSet<>(Arrays.asList(responses));
     }
 
     /**
      * @param exchange  The exchange received by an expectation that will invoke a given response
+     *                  note that processors and predicates must be thread safe
      */
     @Override
     public void process(Exchange exchange) throws Exception {
-        //todo make this threadsafe
-        Iterator<MatchedResponse> responseIterator = responses.iterator();
-
-        while (responseIterator.hasNext()) {
-            MatchedResponse matchedResponse = responseIterator.next();
+        for (MatchedResponse matchedResponse : responses) {
             if (matchedResponse.inputPredicate.matches(exchange)) {
+                logger.debug("Matched input for predicate {} at endpoint {}",
+                        matchedResponse.inputPredicate,exchange.getFromEndpoint());
                 matchedResponse.responseProcessor.process(exchange);
+                return;
             }
         }
 
         logger.warn("The exchange arriving at endpoint {} found no response match for body {}",
                 exchange.getFromEndpoint(), exchange.getIn().getBody(String.class));
-        exchange.getOut().setBody("");
     }
 
     /**
-     * An approach class for making input->output pairs
+     * An approach class for making input->output pairs; these predicates and processors must be thread safe
+     * as they can be called concurrently
      */
     public static class MatchedResponse {
         private Predicate inputPredicate;
         private Processor responseProcessor;
 
-        public MatchedResponse(Predicate inputPredicate, Processor responseProcessor) throws Exception {
+        public MatchedResponse(Predicate inputPredicate, Processor responseProcessor) {
             this.inputPredicate = inputPredicate;
             this.responseProcessor = responseProcessor;
         }
