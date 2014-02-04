c24a1310fbb593565aef20dc43bbb5a983562b22
dmacdonald2013 <d.macdonald@auckland.ac.nz>
2014-02-04 23:08:14 +1300

    fixed for new dsl

--- src/test/java/nz/ac/auckland/integration/tests/validators/HttpExceptionValidatorTest.java
+++ src/test/java/nz/ac/auckland/integration/tests/validators/HttpExceptionValidatorTest.java
@@ -4,10 +4,10 @@ import nz.ac.auckland.integration.testing.resource.HeadersTestResource;
 import nz.ac.auckland.integration.testing.resource.JsonTestResource;
 import nz.ac.auckland.integration.testing.resource.PlainTextTestResource;
 import nz.ac.auckland.integration.testing.resource.TestResource;
-import nz.ac.auckland.integration.testing.validator.HeadersValidator;
-import nz.ac.auckland.integration.testing.validator.HttpErrorValidator;
-import nz.ac.auckland.integration.testing.validator.Validator;
+import nz.ac.auckland.integration.testing.predicate.HeadersPredicate;
+import nz.ac.auckland.integration.testing.predicate.HttpErrorPredicate;
 import org.apache.camel.Exchange;
+import org.apache.camel.Predicate;
 import org.apache.camel.component.http.HttpOperationFailedException;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.DefaultExchange;
@@ -22,97 +22,97 @@ public class HttpExceptionValidatorTest extends Assert {
 
     @Test
     public void testNullExchange() throws Exception {
-        HttpErrorValidator validator = new HttpErrorValidator.Builder().build();
-        assertFalse(validator.validate(null));
+        HttpErrorPredicate validator = new HttpErrorPredicate.Builder().build();
+        assertFalse(validator.matches(null));
     }
 
     @Test
     public void testNullException() throws Exception {
-        HttpErrorValidator validator = new HttpErrorValidator.Builder().build();
+        HttpErrorPredicate validator = new HttpErrorPredicate.Builder().build();
         Exchange e = new DefaultExchange(new DefaultCamelContext());
-        assertFalse(validator.validate(e));
+        assertFalse(validator.matches(e));
     }
 
     @Test
     public void testSetResponseBodyHeadersStatusCode() throws Exception {
-        Validator responseBodyValidator = new Validator() {
+        Predicate responseBodyValidator = new Predicate() {
             @Override
-            public boolean validate(Exchange exchange) {
+            public boolean matches(Exchange exchange) {
                 return true;
             }
         };
 
-        HeadersValidator responseHeadersValidator = new MockHeadersValidator(null, true);
+        HeadersPredicate responseHeadersPredicate = new MockHeadersPredicate(null, true);
 
-        HttpErrorValidator validator = new HttpErrorValidator.Builder()
-                .responseBodyValidator(responseBodyValidator)
-                .responseHeadersValidator(new MockHeadersValidator(null, true))
+        HttpErrorPredicate validator = new HttpErrorPredicate.Builder()
+                .responseBody(responseBodyValidator)
+                .responseHeaders(new MockHeadersPredicate(null, true))
                 .statusCode(500)
                 .build();
 
         assertEquals(validator.getStatusCode(), 500);
-        assertEquals(validator.getResponseBodyValidator(), responseBodyValidator);
-        assertEquals(validator.getResponseHeadersValidator(), responseHeadersValidator);
+        assertEquals(validator.getBodyPredicate(), responseBodyValidator);
+        assertEquals(validator.getHeadersPredicate(), responseHeadersPredicate);
     }
 
     @Test
     public void testWrongException() throws Exception {
         Exchange e = new DefaultExchange(new DefaultCamelContext());
         e.setException(new IOException());
-        assertFalse(new HttpErrorValidator().validate(e));
+        assertFalse(new HttpErrorPredicate().matches(e));
     }
 
     @Test
     public void testDifferentStatusCode() throws Exception {
-        Validator responseBodyValidator = new Validator() {
+        Predicate responseBodyValidator = new Predicate() {
             @Override
-            public boolean validate(Exchange exchange) {
+            public boolean matches(Exchange exchange) {
                 return true;
             }
         };
 
-        HttpErrorValidator validator = new HttpErrorValidator.Builder()
-                .responseBodyValidator(responseBodyValidator)
-                .responseHeadersValidator(new MockHeadersValidator(null, true))
+        HttpErrorPredicate validator = new HttpErrorPredicate.Builder()
+                .responseBody(responseBodyValidator)
+                .responseHeaders(new MockHeadersPredicate(null, true))
                 .statusCode(500)
                 .build();
 
         Exchange e = new DefaultExchange(new DefaultCamelContext());
         e.setException(new HttpOperationFailedException("uri", 123, "status", "location", null, null));
-        assertFalse(validator.validate(e));
+        assertFalse(validator.matches(e));
     }
 
     @Test
     public void testCorrectBody() throws Exception {
-        Validator responseBodyValidator = new Validator() {
+        Predicate responseBodyValidator = new Predicate() {
             @Override
-            public boolean validate(Exchange exchange) {
+            public boolean matches(Exchange exchange) {
                 return exchange.getIn().getBody(String.class).equals("foo");
             }
         };
 
-        HttpErrorValidator validator = new HttpErrorValidator.Builder()
-                .responseBodyValidator(responseBodyValidator)
-                .responseHeadersValidator(new MockHeadersValidator(null, false))
+        HttpErrorPredicate validator = new HttpErrorPredicate.Builder()
+                .responseBody(responseBodyValidator)
+                .responseHeaders(new MockHeadersPredicate(null, false))
                 .build();
 
         Exchange e = new DefaultExchange(new DefaultCamelContext());
         e.setException(new HttpOperationFailedException("uri", 123, "status", "location", null, "foo"));
-        assertFalse(validator.validate(e));
+        assertFalse(validator.matches(e));
     }
 
     @Test
     public void testCorrectHeaders() throws Exception {
-        Validator responseBodyValidator = new Validator() {
+        Predicate responseBodyValidator = new Predicate() {
             @Override
-            public boolean validate(Exchange exchange) {
+            public boolean matches(Exchange exchange) {
                 return false;
             }
         };
 
-        Validator responseHeadersValidator = new Validator() {
+        Predicate responseHeadersPredicate = new Predicate() {
             @Override
-            public boolean validate(Exchange exchange) {
+            public boolean matches(Exchange exchange) {
                 String foo = exchange.getIn().getHeader("foo", String.class);
                 String baz = exchange.getIn().getHeader("baz", String.class);
                 return foo.equals("baz") && baz.equals("foo");
@@ -123,47 +123,47 @@ public class HttpExceptionValidatorTest extends Assert {
         map.put("foo", "baz");
         map.put("baz", "moo");
 
-        HttpErrorValidator validator = new HttpErrorValidator.Builder()
-                .responseBodyValidator(responseBodyValidator)
-                .responseHeadersValidator(new HeadersValidator(new HeadersTestResource((Map) map)))
+        HttpErrorPredicate validator = new HttpErrorPredicate.Builder()
+                .responseBody(responseBodyValidator)
+                .responseHeaders(new HeadersPredicate(new HeadersTestResource((Map) map)))
                 .build();
 
         Exchange e = new DefaultExchange(new DefaultCamelContext());
         e.setException(new HttpOperationFailedException("uri", 123, "status", "location", map, "foo"));
-        assertFalse(validator.validate(e));
+        assertFalse(validator.matches(e));
     }
 
     @Test
     public void testPlainTextResource() throws Exception {
         PlainTextTestResource resource = new PlainTextTestResource("foo");
 
-        HttpErrorValidator validator = new HttpErrorValidator.Builder()
-                .responseBodyValidator(resource).build();
+        HttpErrorPredicate validator = new HttpErrorPredicate.Builder()
+                .responseBody(resource).build();
 
         Exchange e = new DefaultExchange(new DefaultCamelContext());
         e.setException(new HttpOperationFailedException("uri", 123, "status", "location", null, "foo"));
 
-        assertTrue(validator.validate(e));
+        assertTrue(validator.matches(e));
     }
 
     @Test
     public void testJsonResource() throws Exception {
         JsonTestResource resource = new JsonTestResource("{\"foo\":\"baz\"}");
 
-        HttpErrorValidator validator = new HttpErrorValidator.Builder()
-                .responseBodyValidator(resource).build();
+        HttpErrorPredicate validator = new HttpErrorPredicate.Builder()
+                .responseBody(resource).build();
 
         Exchange e = new DefaultExchange(new DefaultCamelContext());
         e.setException(new HttpOperationFailedException("uri", 123, "status", "location", null, "{\"foo\":\"baz\"}"));
 
-        assertTrue(validator.validate(e));
+        assertTrue(validator.matches(e));
     }
 
     @Test
     public void testHeadersResource() throws Exception {
-        Validator responseBodyValidator = new Validator() {
+        Predicate responseBodyValidator = new Predicate() {
             @Override
-            public boolean validate(Exchange exchange) {
+            public boolean matches(Exchange exchange) {
                 return false;
             }
         };
@@ -174,27 +174,28 @@ public class HttpExceptionValidatorTest extends Assert {
 
         HeadersTestResource resource = new HeadersTestResource((Map) map);
 
-        HttpErrorValidator validator = new HttpErrorValidator.Builder()
-                .responseBodyValidator(responseBodyValidator)
-                .responseHeadersValidator(resource)
+        HttpErrorPredicate validator = new HttpErrorPredicate.Builder()
+                .responseBody(responseBodyValidator)
+                .responseHeaders(resource)
                 .build();
 
         Exchange e = new DefaultExchange(new DefaultCamelContext());
         e.setException(new HttpOperationFailedException("uri", 123, "status", "location", map, "foo"));
-        assertFalse(validator.validate(e));
+        assertFalse(validator.matches(e));
     }
 }
 
-class MockHeadersValidator extends HeadersValidator {
+class MockHeadersPredicate extends HeadersPredicate {
 
     private boolean response = true;
 
-    public MockHeadersValidator(TestResource<Map<String, Object>> resource, boolean response) {
+    public MockHeadersPredicate(TestResource<Map<String, Object>> resource, boolean response) {
         super(resource);
         this.response = response;
     }
 
-    public boolean validate(Exchange exchange) {
+    @Override
+    public boolean matches(Exchange exchange) {
         return response;
     }
 }
