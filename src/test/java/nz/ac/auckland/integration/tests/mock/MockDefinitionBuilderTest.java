7479fe82cb5a591e152636551f3e1b62a0629cba
dmacdonald2013 <davidm@cdonald.net.nz>
2014-02-13 16:49:13 +1300

    more tests

--- src/test/java/nz/ac/auckland/integration/tests/mock/MockDefinitionBuilderTest.java
+++ src/test/java/nz/ac/auckland/integration/tests/mock/MockDefinitionBuilderTest.java
@@ -1,10 +1,15 @@
 package nz.ac.auckland.integration.tests.mock;
 
+import nz.ac.auckland.integration.testing.endpointoverride.EndpointOverride;
 import nz.ac.auckland.integration.testing.mock.MockDefinition;
 import nz.ac.auckland.integration.testing.processor.BodyProcessor;
+import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.Predicate;
 import org.apache.camel.Processor;
+import org.apache.camel.component.cxf.CxfEndpoint;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
 import org.apache.camel.model.RouteDefinition;
 import org.junit.Assert;
 import org.junit.Test;
@@ -68,17 +73,6 @@ public class MockDefinitionBuilderTest extends Assert {
                 .mockFeederRoute(rd).build(null);
 
         assertEquals("foo",def.getMockFeederRoute().getGroup());
-    }
-
-    @Test
-    public void testMergeMockFeederRoute() throws Exception {
-
-    }
-
-    @Test
-    public void testMergeOrdering() throws Exception {
-        //endpoint
-        //overall
     }
 
     @Test
@@ -210,6 +204,184 @@ public class MockDefinitionBuilderTest extends Assert {
         assertEquals(2,def1.getPredicates().size());
         assertNotNull(def1.getLenientProcessor());
         assertNotNull(def1.getLenientSelector());
+
+    }
+
+    @Test
+    public void testMergeMockLenientMock() throws Exception {
+        RouteDefinition rd = new RouteDefinition();
+        rd.setGroup("foo");
+
+        MockDefinition def = new MockDefinition.MockDefinitionBuilder("foo").addProcessors(new BodyProcessor(text("foo")))
+                .addProcessors(new BodyProcessor(text("baz"))).addPredicates(text("foo")).addPredicates(text("baz"))
+                .endpointNotOrdered()
+                .ordering(MockDefinition.OrderingType.NONE)
+                .messageResultWaitTime(1234).minimalResultWaitTime(5678).reassertionPeriod(314)
+                .mockFeederRoute(rd).build(null);
+
+        MockDefinition def1 = new MockDefinition.MockDefinitionBuilder("foo").addProcessors(new BodyProcessor(text("a")))
+                .addProcessors(new BodyProcessor(text("b"))).addPredicates(text("a")).addPredicates(text("b")).lenient()
+                .addEndpointOverride(new EndpointOverride() {
+                    @Override
+                    public void overrideEndpoint(Endpoint endpoint) {
+
+                    }
+                }).build(def);
+
+        MockDefinition def2 = new MockDefinition.MockDefinitionBuilder("foo").addProcessors(new BodyProcessor(text("moo")))
+                .endpointNotOrdered().ordering(MockDefinition.OrderingType.NONE)
+                .addProcessors(new BodyProcessor(text("cow"))).addPredicates(text("moo")).addPredicates(text("cow")).build(def1);
+
+        Exchange e = new DefaultExchange(new DefaultCamelContext());
+        e.setFromEndpoint(new CxfEndpoint(""));
+
+        assertEquals(4, def2.getExpectedMessageCount());
+        assertEquals(4,def2.getPredicates().size());
+        assertEquals(4,def2.getProcessors().size());
+        assertEquals(5678 + (4*1234),def2.getResultWaitTime());
+        assertEquals(314,def2.getReassertionPeriod());
+        assertEquals("foo",def2.getMockFeederRoute().getGroup());
+        assertEquals(MockDefinition.OrderingType.NONE,def2.getOrderingType());
+        assertFalse(def2.isEndpointOrdered());
+        assertNotNull(def2.getLenientSelector());
+        assertNotNull(def2.getLenientProcessor());
+
+        e.getIn().setBody("foo");
+        assertTrue(def2.getPredicates().get(0).matches(e));
+        e.getIn().setBody("baz");
+        assertTrue(def2.getPredicates().get(1).matches(e));
+        e.getIn().setBody("moo");
+        assertTrue(def2.getPredicates().get(2).matches(e));
+        e.getIn().setBody("cow");
+        assertTrue(def2.getPredicates().get(3).matches(e));
+
+        def2.getProcessors().get(0).process(e);
+        assertEquals("foo",e.getIn().getBody(String.class));
+        def2.getProcessors().get(1).process(e);
+        assertEquals("baz",e.getIn().getBody(String.class));
+        def2.getProcessors().get(2).process(e);
+        assertEquals("moo",e.getIn().getBody(String.class));
+        def2.getProcessors().get(3).process(e);
+        assertEquals("cow",e.getIn().getBody(String.class));
+
+        assertTrue(def2.getLenientSelector().matches(e));
+
+        //test it cycles through the responses
+        def2.getLenientProcessor().process(e);
+        assertEquals("a",e.getIn().getBody(String.class));
+        def2.getLenientProcessor().process(e);
+        assertEquals("b",e.getIn().getBody(String.class));
+        def2.getLenientProcessor().process(e);
+        assertEquals("a",e.getIn().getBody(String.class));
+        def2.getLenientProcessor().process(e);
+        assertEquals("b",e.getIn().getBody(String.class));
+        def2.getLenientProcessor().process(e);
+        assertEquals("a",e.getIn().getBody(String.class));
+
+        assertEquals(3,def2.getEndpointOverrides().size());
+    }
+
+    @Test
+    public void testMergeMockMockLenient() throws Exception {
+        RouteDefinition rd = new RouteDefinition();
+        rd.setGroup("foo");
+
+        MockDefinition def = new MockDefinition.MockDefinitionBuilder("foo").addProcessors(new BodyProcessor(text("foo")))
+                .addProcessors(new BodyProcessor(text("baz"))).addPredicates(text("foo")).addPredicates(text("baz"))
+                .endpointNotOrdered()
+                .ordering(MockDefinition.OrderingType.NONE)
+                .mockFeederRoute(rd).build(null);
+
+        MockDefinition def1 = new MockDefinition.MockDefinitionBuilder("foo").addProcessors(new BodyProcessor(text("moo")))
+                .endpointNotOrdered().ordering(MockDefinition.OrderingType.NONE)
+                //these should be ignored
+                .messageResultWaitTime(1234).minimalResultWaitTime(5678).reassertionPeriod(314)
+                .addProcessors(new BodyProcessor(text("cow"))).addPredicates(text("moo")).addPredicates(text("cow")).build(def);
+
+        MockDefinition def2 = new MockDefinition.MockDefinitionBuilder("foo").addProcessors(new BodyProcessor(text("a")))
+                .addProcessors(new BodyProcessor(text("b"))).addPredicates(text("a")).addPredicates(text("b")).lenient()
+                .addEndpointOverride(new EndpointOverride() {
+                    @Override
+                    public void overrideEndpoint(Endpoint endpoint) {
+
+                    }
+                }).build(def1);
+
+        Exchange e = new DefaultExchange(new DefaultCamelContext());
+        e.setFromEndpoint(new CxfEndpoint(""));
+
+        assertEquals(4, def2.getExpectedMessageCount());
+        assertEquals(4,def2.getPredicates().size());
+        assertEquals(4,def2.getProcessors().size());
+        assertEquals(10000 + (4*1000),def2.getResultWaitTime());
+        assertEquals(0,def2.getReassertionPeriod());
+        assertEquals("foo",def2.getMockFeederRoute().getGroup());
+        assertEquals(MockDefinition.OrderingType.NONE,def2.getOrderingType());
+        assertFalse(def2.isEndpointOrdered());
+        assertNotNull(def2.getLenientSelector());
+        assertNotNull(def2.getLenientProcessor());
+
+        e.getIn().setBody("foo");
+        assertTrue(def2.getPredicates().get(0).matches(e));
+        e.getIn().setBody("baz");
+        assertTrue(def2.getPredicates().get(1).matches(e));
+        e.getIn().setBody("moo");
+        assertTrue(def2.getPredicates().get(2).matches(e));
+        e.getIn().setBody("cow");
+        assertTrue(def2.getPredicates().get(3).matches(e));
+
+        def2.getProcessors().get(0).process(e);
+        assertEquals("foo",e.getIn().getBody(String.class));
+        def2.getProcessors().get(1).process(e);
+        assertEquals("baz",e.getIn().getBody(String.class));
+        def2.getProcessors().get(2).process(e);
+        assertEquals("moo",e.getIn().getBody(String.class));
+        def2.getProcessors().get(3).process(e);
+        assertEquals("cow",e.getIn().getBody(String.class));
+
+        assertTrue(def2.getLenientSelector().matches(e));
+
+        //test it cycles through the responses
+        def2.getLenientProcessor().process(e);
+        assertEquals("a",e.getIn().getBody(String.class));
+        def2.getLenientProcessor().process(e);
+        assertEquals("b",e.getIn().getBody(String.class));
+        def2.getLenientProcessor().process(e);
+        assertEquals("a",e.getIn().getBody(String.class));
+        def2.getLenientProcessor().process(e);
+        assertEquals("b",e.getIn().getBody(String.class));
+        def2.getLenientProcessor().process(e);
+        assertEquals("a",e.getIn().getBody(String.class));
+
+        assertEquals(3,def2.getEndpointOverrides().size());
+    }
+
+    @Test
+    public void testEmptyProcessorsForLenientProcessor() throws Exception {
+        MockDefinition def = new MockDefinition.MockDefinitionBuilder("foo").lenient().expectedMessageCount(5).build(null);
+
+        assertNotNull(def.getLenientProcessor());
+        assertNotNull(def.getLenientSelector());
+
+        Exchange e = new DefaultExchange(new DefaultCamelContext());
+        e.setFromEndpoint(new CxfEndpoint(""));
+
+        //ensure we can handle the case when no processors are available
+        e.getIn().setBody("foo");
+        def.getLenientProcessor().process(e);
+        assertEquals("foo",e.getIn().getBody(String.class));
+        assertEquals(0,def.getExpectedMessageCount());
+    }
+
+    @Test
+    public void testLenientExcludesExpectedMessages() throws Exception {
+        MockDefinition def = new MockDefinition.MockDefinitionBuilder("foo").lenient().
+                expectedMessageCount(5).addPredicates(text("1")).addPredicates(text("1")).build(null);
+
+        assertNotNull(def.getLenientSelector());
+        assertNotNull(def.getLenientProcessor());
+
+        assertEquals(0,def.getExpectedMessageCount());
 
     }
 
