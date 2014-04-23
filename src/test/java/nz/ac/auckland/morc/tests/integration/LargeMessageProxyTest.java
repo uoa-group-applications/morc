093821f162025d34d4a34be3f4ec7bd5ce5c84ae
dmacdonald2013 <davidm@cdonald.net.nz>
2014-04-24 11:27:30 +1200

    added test to show large messages fail with CXF (probably to do with streaming and http chunked encoding)

---
+++ src/test/java/nz/ac/auckland/morc/tests/integration/LargeMessageProxyTest.java
@@ -0,0 +1,34 @@
+package nz.ac.auckland.morc.tests.integration;
+
+import nz.ac.auckland.morc.MorcTestBuilder;
+import org.apache.camel.builder.RouteBuilder;
+
+//A quick test to ensure large messages are streamed correctly
+public class LargeMessageProxyTest extends MorcTestBuilder {
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                //a straight through proxy
+                from("cxf:bean:pingService")
+                        .to("cxf:http://localhost:9090/pingService?wsdlURL=data/PingService.wsdl&dataFormat=PAYLOAD");
+            }
+        };
+    }
+
+    @Override
+    public String[] getSpringContextPaths() {
+        return new String[]{"large-message-context.xml"};
+    }
+
+    @Override
+    public void configure() {
+        syncTest("Large Message Proxy Test","cxf:http://localhost:8090/pingService")
+                .requestBody(xml(classpath("/data/bigPingRequest.xml")))
+                .expectedResponseBody(xml(classpath("/data/bigPingResponse.xml")))
+                .addExpectation(syncExpectation("cxf:http://localhost:9090/pingService?wsdlURL=data/PingService.wsdl")
+                    .expectedBody(xml(classpath("/data/bigPingRequest.xml")))
+                    .responseBody(xml(classpath("/data/bigPingResponse.xml"))));
+    }
+}
