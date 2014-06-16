6af78f94b1ec95035ef0f50ea5ff8a7175963571
dmacdonald2013 <davidm@cdonald.net.nz>
2014-06-16 20:51:45 +1200

    completed test

--- src/test/java/nz/ac/auckland/morc/tests/processor/BodyProcessorTest.java
+++ src/test/java/nz/ac/auckland/morc/tests/processor/BodyProcessorTest.java
@@ -3,25 +3,28 @@ package nz.ac.auckland.morc.tests.processor;
 import nz.ac.auckland.morc.MorcTestBuilder;
 import nz.ac.auckland.morc.processor.BodyProcessor;
 import nz.ac.auckland.morc.resource.PlainTextTestResource;
+import nz.ac.auckland.morc.utility.XmlUtilities;
 import org.apache.camel.Exchange;
+import org.apache.camel.component.cxf.CxfEndpoint;
+import org.apache.camel.component.cxf.cxfbean.CxfBeanEndpoint;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.DefaultExchange;
 import org.junit.Assert;
 import org.junit.Test;
+import org.w3c.dom.Document;
 
 public class BodyProcessorTest extends Assert {
 
     @Test
     public void testXmlRuntimeTestResource() throws Exception {
-        MorcTestBuilder.XmlRuntimeTestResource<PlainTextTestResource> resource =
-                new MorcTestBuilder.XmlRuntimeTestResource<>(new PlainTextTestResource("foo"));
+        MorcTestBuilder.XmlRuntimeTestResource resource =
+                new MorcTestBuilder.XmlRuntimeTestResource(new PlainTextTestResource("<foo/>"));
 
         Exchange e = new DefaultExchange(new DefaultCamelContext());
-
+        e.setFromEndpoint(new CxfEndpoint(""));
         BodyProcessor processor = new BodyProcessor(resource);
 
         processor.process(e);
-        assertEquals("foo",e.getIn().getBody(String.class));
-
+        assertEquals("<foo/>",e.getIn().getBody(String.class));
     }
 }
