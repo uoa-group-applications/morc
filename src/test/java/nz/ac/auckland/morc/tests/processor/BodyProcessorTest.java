c52b818d81d0b35388e5cf11fe67b88703b27b22
dmacdonald2013 <davidm@cdonald.net.nz>
2014-06-16 20:39:13 +1200

    A test to show things are broken

---
+++ src/test/java/nz/ac/auckland/morc/tests/processor/BodyProcessorTest.java
@@ -0,0 +1,27 @@
+package nz.ac.auckland.morc.tests.processor;
+
+import nz.ac.auckland.morc.MorcTestBuilder;
+import nz.ac.auckland.morc.processor.BodyProcessor;
+import nz.ac.auckland.morc.resource.PlainTextTestResource;
+import org.apache.camel.Exchange;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.DefaultExchange;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BodyProcessorTest extends Assert {
+
+    @Test
+    public void testXmlRuntimeTestResource() throws Exception {
+        MorcTestBuilder.XmlRuntimeTestResource<PlainTextTestResource> resource =
+                new MorcTestBuilder.XmlRuntimeTestResource<>(new PlainTextTestResource("foo"));
+
+        Exchange e = new DefaultExchange(new DefaultCamelContext());
+
+        BodyProcessor processor = new BodyProcessor(resource);
+
+        processor.process(e);
+        assertEquals("foo",e.getIn().getBody(String.class));
+
+    }
+}
