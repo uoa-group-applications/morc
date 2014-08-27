2eae950eda01f78b475c8790d4d9797d5e3156e5
dmacdonald2013 <davidm@cdonald.net.nz>
2014-08-27 20:31:10 +1200

    attempt at automatically generating javadoc for gh_pages

---
+++ .ci/javadoc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#borrowed from ReadyTalk/swt-bling
+
+if [ "$TRAVIS_REPO_SLUG" == "uoa-group-applications/morc" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+
+  mvn javadoc:javadoc
+  cp -R target/site/apidocs $HOME/apidocs-latest
+
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/uoa-group-applications/morc gh-pages > /dev/null
+
+  cd gh-pages
+  git rm -rf ./apidocs
+  cp -Rf $HOME/apidocs-latest ./apidocs
+  git add -f .
+  git commit -m "Adding SNAPSHOT apidocs $TRAVIS_BUILD_NUMBER to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+  
+fi
